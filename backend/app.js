993f3fda4941adab930e692cc86ffc5db9a07883
ali mughal <alimghal41@gmail.com>
2025-10-01 20:54:13 +0500

    app.js updated

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,10 @@
-
-
+// app.js
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { connectDatabase } from "./config/database.js";
 import messageRoutes from "./routes/messageRoutes.js";
+import cvRoutes from "./routes/cvRoutes.js";
 
 dotenv.config();
 
@@ -25,12 +25,41 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Serve static files (for CV)
+app.use(express.static("public"));
+
 // Routes
 app.use("/api/v1/message", messageRoutes);
+app.use("/api/v1/cv", cvRoutes);
 
 // Root route
 app.get("/", (req, res) => {
   res.send("Portfolio Backend API is running!");
+});
+
+// Health check route
+app.get("/api/v1/health", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Server is healthy",
+  });
+});
+
+// 404 handler
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "Route not found",
+  });
+});
+
+// Error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({
+    success: false,
+    message: "Something went wrong!",
+  });
 });
 
 export default app;
